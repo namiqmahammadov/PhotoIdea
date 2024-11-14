63c38583011cd2947f127d8345f870a70e533496
namiqmahammadov <130308957+namiqmahammadov@users.noreply.github.com>
2024-11-14 17:46:13 -0500

    Initial commit

---
+++ src/main/java/com/namiq/PhotoIdea/PhotoIdeaApplication.java
@@ -0,0 +1,13 @@
+package com.namiq.PhotoIdea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoIdeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoIdeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/namiq/PhotoIdea/PhotoIdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namiq.PhotoIdea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoIdeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
